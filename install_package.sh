a450c856c76d06512d85fdc176d3e59b8e6651f5
Kyle Thompson <kat0028@auburn.edu>
2023-08-08 13:49:31 -0500

    aded removable of .obj-... files

--- install_package.sh
+++ install_package.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 
+rosdep update
+
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro noetic
 fakeroot debian/rules binary
 sudo dpkg -i ../*.deb 
 rm ../*.deb
 rm -r debian
+rm -r .obj* 
